234c1b36a3429afbf2ceb2f3757394d4e02628a1
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2024-02-27 15:21:46 +0100

    Docker container with flask

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Email
+import email_validator
+
+class InputForm(FlaskForm):
+    user_input = StringField('Enter name:', validators=[DataRequired()])
+    description = TextAreaField('Enter surname:', validators=[DataRequired()])
+    email = EmailField('Enter e-mail:', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
